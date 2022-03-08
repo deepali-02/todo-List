a4b210c5169a70aeb62c80ade2ee50f658e3736e
deepali-02 <deepali93.redekar@gmail.com>
2022-03-08 13:50:34 +0100

    declare states and write function hadleChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { ChangeEvent, FC, useState } from "react";
+import moment from "moment";
 
 import "./App.css";
 
-const App = () => {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<string>(Date());
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadline(event.target.value);
+    }
+  };
   return (
     <div className="App">
       <h1>ToDo List </h1>
       <div className="header">
         <div className="inputContainer">
-          <input type= "text" placeholder="add a task" />
-          <input type= "date" placeholder="set deadline" />
+          <input
+            type="text"
+            placeholder="add a task"
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="date"
+            placeholder="set deadline"
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
         </div>
         <button>Add Task</button>
       </div>

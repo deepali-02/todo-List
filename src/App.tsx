2a7149eea68bde7cdd0cedbb5c80aa3528061e91
deepali-02 <deepali93.redekar@gmail.com>
2022-03-08 11:59:42 +0100

    create input field and button to add a task

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      <h1>ToDo List </h1>
+      <div className="header">
+        <div className="inputContainer">
+          <input type= "text" placeholder="add a task" />
+          <input type= "date" placeholder="set deadline" />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
     </div>
   );
-}
+};
 
 export default App;

a096ddde70af9fde08ca6c7fbb1073c43c148e05
deepali-02 <deepali93.redekar@gmail.com>
2022-03-14 12:26:21 +0100

    display task

--- src/Component/ToDoList.tsx
+++ src/Component/ToDoList.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { getAllJSDocTagsOfKind } from "typescript";
+import {ITask} from "../interfaces"
 
-export default function ToDoList() {
+interface Props {
+    task:ITask
+}
+
+export default function ToDoList({task}:Props) {
   return (
     <div>
-      <h1>Tasks</h1>
+     {task.taskName} {task.deadline}
     </div>
   );
 }
